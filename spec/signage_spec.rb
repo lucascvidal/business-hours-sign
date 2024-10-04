9f28da4cb49b86dd85b3dc77f138a82fc42f3444
Lucas Vidal <lcv55@hotmail.com>
2024-10-04 23:46:29 +0000

    Add specs for second and third scenarios

--- spec/signage_spec.rb
+++ spec/signage_spec.rb
@@ -10,4 +10,25 @@ RSpec.describe Signage do
 
     expect(r.pretty_list).to eq(['Monday: 2:00 - 5:00', 'Tuesday: 3:00 - 4:00', 'Thursday: 2:00 - 4:00'])
   end
+
+  it 'outputs consecutive days with the same times separated by a comma' do
+    list = []
+    list << Day.new('Monday', '2:00', '5:00')
+    list << Day.new('Tuesday', '2:00', '5:00')
+    list << Day.new('Thursday', '2:00', '4:00')
+    r = Signage.new(list)
+
+    expect(r.pretty_list).to eq(['Monday, Tuesday: 2:00 - 5:00', 'Thursday: 2:00 - 4:00'])
+  end
+
+  it 'outputs three or more consecutive days with the same time separated by a dash' do
+    list = []
+    list << Day.new('Monday', '2:00', '5:00')
+    list << Day.new('Tuesday', '2:00', '5:00')
+    list << Day.new('Wednesday', '2:00', '5:00')
+    list << Day.new('Friday', '2:00', '4:00')
+    r = Signage.new(list)
+
+    expect(r.pretty_list).to eq(['Monday - Wednesday: 2:00 - 5:00', 'Friday: 2:00 - 4:00'])
+  end
 end
