607919ac9f07e5b3665dc68d5f09975b0d6733ea
Lucas Vidal <lcv55@hotmail.com>
2024-10-05 00:22:49 +0000

    Update pretty_list method to fulfill specs

--- signage.rb
+++ signage.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class Day
   attr_reader :name, :start_time, :end_time
 
@@ -16,8 +18,37 @@ class Signage
   end
 
   def pretty_list
-    list.map do |day|
-      "#{day.name}: #{day.start_time} - #{day.end_time}"
+    sorted_days = sort_days(@list)
+    grouped_days = group_days_by_time(sorted_days)
+    format_grouped_days(grouped_days)
+  end
+
+  private
+
+  def sort_days(days)
+    days_of_week = Date::DAYNAMES.rotate(1)
+    days.sort_by { |day| days_of_week.index(day.name) }
+  end
+
+  def group_days_by_time(sorted_days)
+    sorted_days.slice_when do |prev, curr|
+      "#{prev.start_time} - #{prev.end_time}" != "#{curr.start_time} - #{curr.end_time}"
+    end
+  end
+
+  def format_grouped_days(grouped_days)
+    grouped_days.map do |group|
+      format_days(group.map(&:name), "#{group.first.start_time} - #{group.first.end_time}")
+    end
+  end
+
+  def format_days(days, time_range)
+    if days.size > 2
+      "#{days.first} - #{days.last}: #{time_range}"
+    elsif days.size == 2
+      "#{days.join(', ')}: #{time_range}"
+    else
+      "#{days.first}: #{time_range}"
     end
   end
 end
