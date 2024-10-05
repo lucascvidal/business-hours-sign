8967ac3db79bc9a54f8b86f6823bbb007282dcc4
Lucas Vidal <lcv55@hotmail.com>
2024-10-05 00:04:25 +0000

    Add spec for out of order list

--- spec/signage_spec.rb
+++ spec/signage_spec.rb
@@ -1,10 +1,20 @@
 require_relative '../signage'
 
-RSpec.describe Signage do
+RSpec.describe Signage do # rubocop:disable Metrics/BlockLength
   it 'outputs different day and time information for store open hours sign' do
     list = []
     list << Day.new('Monday', '2:00', '5:00')
     list << Day.new('Tuesday', '3:00', '4:00')
+    list << Day.new('Thursday', '2:00', '4:00')
+    r = Signage.new(list)
+
+    expect(r.pretty_list).to eq(['Monday: 2:00 - 5:00', 'Tuesday: 3:00 - 4:00', 'Thursday: 2:00 - 4:00'])
+  end
+
+  it 'outputs different day and time information for store open hours sign even if days are not sorted' do
+    list = []
+    list << Day.new('Tuesday', '3:00', '4:00')
+    list << Day.new('Monday', '2:00', '5:00')
     list << Day.new('Thursday', '2:00', '4:00')
     r = Signage.new(list)
 
